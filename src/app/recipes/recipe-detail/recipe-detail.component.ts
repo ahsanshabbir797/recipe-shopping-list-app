f716ea8a320f9b670749fa79218a75b636bc1d4e
ahsanshabbir797 <ahsanshabbir797@gmail.com>
2023-12-13 13:48:37 -0500

    added recipes feature

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailComponent implements OnInit{
+  recipe:Recipe;
+
+  constructor(private recipeService:RecipeService) {
+    this.recipeService.recipeSelected.subscribe((recipeEl:Recipe) => this.recipe=recipeEl)
+  }
+
+  ngOnInit(): void {
+  }
+}
