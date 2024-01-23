3862af505b821eaff575ae41c18c861183fe1387
ahsanshabbir797 <ahsanshabbir797@gmail.com>
2024-01-23 10:42:17 -0500

    modified cross-component communication with subjects

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
-import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { Subject } from "rxjs";
 
 export class ShoppingListService {
-    IngredientsChanged = new EventEmitter<Ingredient[]>();
+    IngredientsChanged = new Subject<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples',5),
         new Ingredient('Tomatoes',10)
@@ -14,11 +14,11 @@ export class ShoppingListService {
 
     addIngredient(ingredient:Ingredient) {
         this.ingredients.push(ingredient);
-        this.IngredientsChanged.emit(this.ingredients.slice());
+        this.IngredientsChanged.next(this.ingredients.slice());
     }
 
     addRecipeIngredients(ingredients:Ingredient[]) {
         this.ingredients.push(...ingredients);
-        this.IngredientsChanged.emit(this.ingredients.slice());
+        this.IngredientsChanged.next(this.ingredients.slice());
     }
 }
